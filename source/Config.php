c03192ba0c21b2dfb19a2abf1cf884c8f82c56b4
jomenaalves <72822496+jomenaalves@users.noreply.github.com>
2021-06-11 21:47:31 -0300

    Update Config.php

--- source/Config.php
+++ source/Config.php
@@ -1,9 +1,9 @@
 <?php 
-    define("PAYPAL_SECRET_LIVE", "EF35j6U5ZpgehFloezkZeFC_7xDlOB2uXJ2eaX4nmEYJCuRIUorVmwHdVrqX3pPpiduoxSq7Q0-BgmRv");
-    define("PAYPAL_CLIENT_ID_LIVE", "AdjWomgp5QvrgUrIXvQ0gtbOldM76LaFx2qBwoLmNpk_Fm0PzMCNs_FYuAN4ZgyHOFDLA9O7Ygwmpfi7");
+    define("PAYPAL_SECRET_LIVE", "");
+    define("PAYPAL_CLIENT_ID_LIVE", "");
 
-    define("PAYPAL_SECRET_SANDBOX", "EPy612_g0d_0G1NQmK7891i-3RWhLgU903PjRZGcuVb7NqFSKjDdje45PbJOrQRa4y-z7JnuL51TZYZZ");
-    define("PAYPAL_CLIENT_ID_SANDBOX", "ATlYIITjzhbnFDuDccjIsdmJMvFI-y3tzSFePzQYH6_B8KMxEygjKhAF0EYqN9Jf86s2xIEwvDK14fmc");
+    define("PAYPAL_SECRET_SANDBOX", "");
+    define("PAYPAL_CLIENT_ID_SANDBOX", "");
     
 
 
